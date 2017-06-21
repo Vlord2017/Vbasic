96774573e3c57b0926de44f69bba7fe93280cc4f
Vlord2017 <vlord2005@hotmail.com>
2017-06-21 10:56:44 -0600

    mm

--- WindowsApplication1/WindowsApplication1/save.Designer.vb
+++ WindowsApplication1/WindowsApplication1/save.Designer.vb
@@ -20,6 +20,7 @@ Partial Class save
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
         'Label1
@@ -31,11 +32,21 @@ Partial Class save
         Me.Label1.TabIndex = 0
         Me.Label1.Text = "Label1"
         '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(77, 147)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(157, 58)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
         'save
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(282, 253)
+        Me.ClientSize = New System.Drawing.Size(587, 405)
+        Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.Label1)
         Me.Name = "save"
         Me.Text = "save"
@@ -44,4 +55,5 @@ Partial Class save
 
     End Sub
     Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Button1 As System.Windows.Forms.Button
 End Class
--- WindowsApplication1/WindowsApplication1/save.vb
+++ WindowsApplication1/WindowsApplication1/save.vb
@@ -1,3 +1,8 @@
 ﻿Public Class save 
 
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim report As XtraReport1
+
+
+    End Sub
 End Class
